1ca9bf1d5b8b4cd97e8cd6d64e97279c7cccefd4
Alex Knowles <alexander.knowles@gmail.com>
2017-05-26 08:21:54 -0500

    Respond with an appropriate status reason.
    Define a private function to map a status code
    with a reason.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -42,7 +42,7 @@ defmodule Servy.Handler do
 
   def format_response(conv) do
     """
-    HTTP/1.1 #{conv.status} OK
+    HTTP/1.1 #{conv.status} #{conv.status |> status_reason}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
@@ -50,6 +50,16 @@ defmodule Servy.Handler do
     """
   end
 
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 
 end
 
