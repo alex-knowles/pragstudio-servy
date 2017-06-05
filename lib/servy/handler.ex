6634002a230fdefdd6bb8545d56f98f724b904f9
Alex Knowles <alexander.knowles@gmail.com>
2017-06-05 17:28:21 -0500

    Factor out Servy.FileHandler module.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -5,6 +5,7 @@ defmodule Servy.Handler do
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
 
   @doc "Handle a given request and returns a response."
   def handle(request) do
@@ -30,18 +31,6 @@ defmodule Servy.Handler do
     |> Path.join("form.html")
     |> File.read
     |> handle_file(conv)
-  end
-
-  def handle_file({:ok, content}, conv) do
-    %{conv | status: 200, resp_body: content}
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{conv | status: 404, resp_body: "File not found!"}
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   def route(%{method: "GET", path: "/bears/" <> id} = conv) do
