346f7c9526b331e36480c8328628ad733f2a2828
Alex Knowles <alexander.knowles@gmail.com>
2017-06-05 17:20:46 -0500

    Relocate Servy.Plugins to its own file.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,24 +1,3 @@
-defmodule Servy.Plugins do
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
-    %{conv | path: "/bears/#{id}"}
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def log(conv), do: IO.inspect conv
-
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts "Warning: #{path} is on the loose!"
-    conv
-  end
-
-  def track(conv), do: conv
-end
-
 defmodule Servy.Handler do
   @moduledoc "Handles HTTP requests."
 
